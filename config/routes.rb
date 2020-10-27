430f5169e3bee86d7ae4d765466b3a84be4b27a7
patrick-rush <patrick.a.rush@gmail.com>
2020-10-26 23:04:39 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :products, only: [:index, :create]
+
+  # get 'products/index', to: 'products#index'
+  post 'products', to: 'products#add'
+
 end
